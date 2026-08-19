13f09a5307fda49e90ac5004d55d72f7900da5b6
Tawan67 <tawankhuntha@gmail.com>
2026-08-20 00:05:13 +0700

    add share pattern & validate

---
+++ shared/masked_and_pattern/pattern.py
@@ -0,0 +1,39 @@
+import re
+
+# Raw string patterns
+CREDIT_CARD_PATTERN = (
+    r"^[0-9]{4}-[0-9]{4}-[0-9]{4}-"
+    r"[0-9]{4}$"
+)
+
+EMAIL_PATTERN = (
+    r"^[a-zA-Z0-9]+([._%+-][a-zA-Z0-9]+)*"
+    r"@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+)
+
+PHONE_NUMBER_PATTERN = (
+    r"^[0-9]{3}-[0-9]{3}-"
+    r"[0-9]{4}$"
+)
+
+DOB_PATTERN = (
+    r"^DOB:(0[1-9]|[12][0-9]|3[01])/"
+    r"(0[1-9]|1[0-2])/"
+    r"([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]|[0-9][1-9][0-9]{2}|[1-9][0-9]{3})$"
+)
+
+ADDRESS_PATTERN = (
+    r'^Address:\s*'
+    r'\d+(?:/\d+)?\s+'
+    r'(?:ซอย.+?\s+)?'
+    r'ถนน.+?\s+'
+    r'(?:แขวง|ตำบล).+?\s+'
+    r'(?:เขต|อำเภอ).+?\s+'
+    r'.+$'
+)
+
+CREDIT_CARD_REGEX = re.compile(CREDIT_CARD_PATTERN)
+EMAIL_REGEX = re.compile(EMAIL_PATTERN)
+PHONE_NUMBER_REGEX = re.compile(PHONE_NUMBER_PATTERN)
+DOB_REGEX = re.compile(DOB_PATTERN)
+ADDRESS_REGEX = re.compile(ADDRESS_PATTERN)
--- src/masking_data/serializers.py
+++ src/masking_data/serializers.py
@@ -1,9 +1,42 @@
 from rest_framework import serializers
-
 from .models import MaskingData
+from shared.masked_and_pattern.pattern import (
+    EMAIL_REGEX,
+    PHONE_NUMBER_REGEX,
+    DOB_REGEX,
+    ADDRESS_REGEX,
+    CREDIT_CARD_REGEX
+    )
 
 
 class MaskingDataSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = MaskingData
 		fields = ['id']
+class MaskingDataCreateSerializer(serializers.ModelSerializer):
+    
+	class Meta:
+		model = MaskingData
+		fields = ['email','phone_number','dob','address','credit_card']
+
+	
+	def validate(self, attrs):
+		errors = {}
+
+		if not EMAIL_REGEX.fullmatch(attrs.get('email', '')):
+			errors['email'] = 'Invalid email format.'
+		if not PHONE_NUMBER_REGEX.fullmatch(attrs.get('phone_number', '')):
+			errors['phone_number'] = 'Invalid phone number format.'
+		if not CREDIT_CARD_REGEX.fullmatch(attrs.get('credit_card', '')):
+			errors['credit_card'] = 'Invalid credit card format.'
+		if not ADDRESS_REGEX.fullmatch(attrs.get('address', '')):
+			errors['address'] = 'Invalid address format.'
+		if not DOB_REGEX.fullmatch(attrs.get('dob', '')):
+			errors['dob'] = 'Invalid date of birth format.'
+
+		if errors:
+			raise serializers.ValidationError(errors)
+
+		return attrs
+	
+    
