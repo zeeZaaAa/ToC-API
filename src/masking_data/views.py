dd3394a6be9231a4cc0f0cb009944c52c00f92aa
Tawan67 <tawankhuntha@gmail.com>
2026-08-21 23:18:21 +0700

    create on /query/creat_masking_data validate time by date time (and cannot be future)

--- src/masking_data/views.py
+++ src/masking_data/views.py
@@ -7,6 +7,8 @@ from src.credit_cards.serializers import MaskingCreditCardCreateSerializer
 from .models import MaskingData
 from .serializers import MaskingDataCreateSerializer, MaskingDataSerializer
 from django.db import transaction
+from src.masking_data.query.create_masking_data import create_masking_data
+
 
 class MaskingDataView(APIView):
 	def get(self, request):
@@ -18,16 +20,19 @@ class MaskingDataView(APIView):
 		return Response(serializer.data)
 
 	def post(self, request):
+     
+		user = request.user
+		card_serializer = MaskingCreditCardCreateSerializer(data=request.data.get('credit_card', {}))
+		masking_data_serializer = MaskingDataCreateSerializer(data=request.data)
 		card_serializer = MaskingCreditCardCreateSerializer(data=request.data.get('credit_card'))
 		masking_data_serializer = MaskingDataCreateSerializer(data=request.data)
+  
 		if not card_serializer.is_valid():
 			return Response(card_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 		if not masking_data_serializer.is_valid():
 			return Response(masking_data_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 		try:
-			with transaction.atomic():
-				credit_card = card_serializer.save()
-				masking_data = masking_data_serializer.save(credit_card=credit_card)
+			create_masking_data(card_data=card_serializer.validated_data,masking_data=masking_data_serializer.validated_data,user=user)
 		except Exception as e:
 			return Response(
                 {"error": "Failed to create data", "details": str(e)}, 
