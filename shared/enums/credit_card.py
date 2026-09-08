6ca0de1dec2ec87599b984248b4540a06bc5c7c4
zeeZaaAa <supp.chakorn@gmail.com>
2026-09-09 00:17:32 +0700

    refactor: masking data queries and views to enhance pagination and exclude deleted records

--- shared/enums/credit_card.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class CardStatus(models.TextChoices):
-    ACTIVE = 'ACTIVE', 'Active'
-    DELETED = 'DELETED', 'Deleted'
--- src/masking_data/queries/masking_data_queries.py
+++ src/masking_data/queries/masking_data_queries.py
@@ -1,17 +1,17 @@
 from django.shortcuts import get_object_or_404
 
-from ..models import MaskingData
+from shared.enums.masking_data import DataStatus
 
-# def get_masking_data_by_id(masking_data_id):
-# 	return get_object_or_404(MaskingData, id=masking_data_id)
+from ..models import MaskingData
 
 
 def get_masking_data_by_id(user, masking_data_id):
-    return get_object_or_404(MaskingData, id=masking_data_id, user=user)
+    queryset = MaskingData.objects.exclude(status=DataStatus.DELETED)
+    return get_object_or_404(queryset, id=masking_data_id, user=user)
 
 
 def get_user_masking_data_list(user):
-    return MaskingData.objects.filter(user=user)
+    return MaskingData.objects.filter(user=user).exclude(status=DataStatus.DELETED)
 
 
 def update_masking_data(instance, **fields):
