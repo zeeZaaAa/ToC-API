11214c60dcc33858dbfa649cbf9f28e733eb23a3
zeeZaaAa <supp.chakorn@gmail.com>
2026-09-17 22:03:09 +0700

    feat: update Docker and settings configuration for static files and Google redirect URI

--- config/settings.py
+++ config/settings.py
@@ -179,3 +179,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
